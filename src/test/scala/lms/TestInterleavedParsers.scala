47708babb4ee8d1ad369c4c62ad605b8f32d533b
Sandro Stucki <sandro.stucki@gmail.com>
2013-11-15 21:42:46 +0100

    WIP: Implementing chunked/interleaved tests.

--- src/test/scala/lms/TestInterleavedParsers.scala
+++ src/test/scala/lms/TestInterleavedParsers.scala
@@ -23,7 +23,7 @@ trait Chunker extends NewDesignParsers {
   def nextChunkLength(in: Rep[Input]): Parser[Int]
 }
 
-trait Chunked extends NewDesignParsers {
+trait InterleavedParsers extends NewDesignParsers {
 
   import ParserWorld._
 
@@ -47,7 +47,7 @@ trait Chunked extends NewDesignParsers {
    * process the next chunk in the input.
    */
   override def acceptIf(in: Rep[Input], p: Rep[Elem] => Rep[Boolean]) = Parser[Char] { i =>
-
+    println(publicUnit("i: ") + i + publicUnit(", chunkEnd: ") + chunkEnd)
     if (i >= chunkEnd) { // reached end of chunk
 
       // Parse the length of the next chunk and continue parsing the
@@ -61,17 +61,15 @@ trait Chunked extends NewDesignParsers {
 
   // FIXME: This parser will most likely never be called recursively.
   // We're just exploiting the 'rec' combinator to generate a
-  // top-level function for this parser.
-  private def updateChunkEnd(in: Rep[Input]): Parser[Int] =
-    rec("chunkLength", success(publicUnit(0)) >> { _ =>
-      // Parse the next chunk length using the chunker and adjust the
-      // end-of-chunk pointer.
-      Parser { i => chunker.nextChunkLength(in)(i) } >> { octets =>
-        Parser { i =>
-          chunkEnd = i + octets
-          val s = success(octets)
-          s(i)
-        }
+  // top-level function for it.
+  private def updateChunkEnd(in: Rep[Input]): Parser[Int] = rec("chunkLength",
+    // Parse the next chunk length using the chunker and adjust the
+    // end-of-chunk pointer.
+    Parser { i => chunker.nextChunkLength(in)(i) } >> { octets =>
+      Parser { i =>
+        chunkEnd = i + octets
+        val s = success(octets)
+        s(i)
       }
     })
 
@@ -89,14 +87,53 @@ trait Chunked extends NewDesignParsers {
     }
 }
 
-class JsonTest(val chunker: Chunker) extends Chunked {
+trait WordCounter extends NewDesignParsers {
 
   import ParserWorld._
 
-  def apply(in: Rep[Input]) = { i: Rep[Int] =>
-    val p = initChunkLength(in) ~> rep(word(in) <~ whitespaces(in))
-    p(i)
-  }
+  /**
+   * Check if a given character is white space.
+   *
+   * This method accepts the same set of white space characters as the
+   * C standard library `isspace` function from '<ctype.h>' when used
+   * in the "C" or "POXIX" locale.  According to the `isspace(3)`
+   * manual page, these are
+   *
+   *  - space,
+   *  - form-feed ('\f'),
+   *  - newline ('\n'),
+   *  - carriage return ('\r'),
+   *  - horizontal tab ('\t'), and
+   *  - vertical tab ('\13').
+   */
+  def isSpace(c: Rep[Char]): Rep[Boolean] =
+    c == publicUnit(' ')  || c == publicUnit('\f') ||
+    c == publicUnit('\n') || c == publicUnit('\r') ||
+    c == publicUnit('\t') || c == publicUnit('\13')
+  def isNotSpace(c: Rep[Char]) = !isSpace(c)
+
+  def skipWhile(in: Rep[Input], p: Rep[Elem] => Rep[Boolean]): Parser[Unit] =
+    repFold(acceptIf(in, p))(
+      publicUnit(()), {(_: Rep[Unit], c: Rep[Elem]) => println(publicUnit("c: ") + c); publicUnit(())})
+
+  def skipSpace(in: Rep[Input]): Parser[Unit] = skipWhile(in, isSpace _) ^^ { _: Rep[Unit] => println(publicUnit("end skipSpace")); publicUnit(()) }
+  def skipWord(in: Rep[Input]): Parser[Unit] =
+    (acceptIf(in, isNotSpace _) ^^ { c: Rep[Char] => println(publicUnit("c: ") + c); publicUnit(()) }) ~> skipWhile(in, isNotSpace _) ^^ { _: Rep[Unit] => println(publicUnit("end skipWord")); publicUnit(()) }
+
+  def wordCount(in: Rep[Input], acc: Rep[Int]): Parser[Int] =
+    skipSpace(in) ~> repFold(skipWord(in) <~ skipSpace(in))(
+      acc, {(i: Rep[Int], _) => println(publicUnit("i: ") + (i + 1)); i + 1})
+
+  def apply(in: Rep[Input], acc: Rep[Int]) = wordCount(in, acc)
+}
+
+class InterleavedWordCounter(val chunker: Chunker)
+    extends WordCounter with InterleavedParsers {
+
+  import ParserWorld._
+
+  override def apply(in: Rep[Input], acc: Rep[Int]) =
+    initChunkLength(in) ~> wordCount(in, acc)
 }
 
 object HTTPTestProg extends Chunker {
@@ -120,13 +157,40 @@ object HTTPTestProg extends Chunker {
       else success(octets)
     }
 
-  def testChunked(in: Rep[Array[Char]]): Rep[Unit] = {
-    var s = Failure[List[String]](publicUnit(-1))
-    val p = new JsonTest(this)(in)(publicUnit(0))
-    p{ x => s = x }
-    println(s)
+  def copy(in: Rep[Input], out: Rep[Input], pos: Rep[Int],
+    length: Rep[Int]) = Parser[Int] { i =>
+    for (j <- publicUnit(0) until length) { out(pos + j) = in(i + j) }
+    val s = success(pos + length)
+    s(i + length)
   }
 
+  def testChunked(in: Rep[Input]): Rep[Unit] = {
+
+    // We need a buffer to store chunks in
+    val buf = NewArray[Char](in.length)
+    val end = var_new(publicUnit(0))
+
+    // Parse chunk lengths and copy chunks into the buffer.
+    val p = firstChunkLength(in) >> { len =>
+      copy(in, buf, publicUnit(0), len) } >> { e1 =>
+      end = e1
+      repFold(nextChunkLength(in) >> { len => copy(in, buf, end, len) })(
+        publicUnit(()), { (_: Rep[Unit], e2: Rep[Int]) => end = end + e2; publicUnit(()) })
+    }
+    p(publicUnit(0)){ _ => publicUnit(()) }
+
+    var r = Failure[Int](publicUnit(0))
+    val wc = new WordCounter {}
+    wc(buf, publicUnit(0))(publicUnit(0)){ x => r = x }
+    println(r)
+  }
+
+  def testInterleaved(in: Rep[Array[Char]]): Rep[Unit] = {
+    var r = Failure[Int](publicUnit(0))
+    val wc = new InterleavedWordCounter(this)(in, publicUnit(0))(publicUnit(0))
+    wc{ x => r = x }
+    println(r)
+  }
 }
 
 class InterleavedTest extends FileDiffSuite {
@@ -134,33 +198,8 @@ class InterleavedTest extends FileDiffSuite {
 
   val prefix = "test-out/"
 
-  def testInterleaved = {
+  def testChunkedInterleaved = {
     withOutFile(prefix + "interleaved"){
-
-      ParserWorld.codegen.emitSource(
-        testChunked _,
-        "testChunked",
-        new java.io.PrintWriter(System.out)
-      )
-
-      // A HTTP message
-      val httpMessage =
-      """|HTTP/1.1 418 I'm a teapot
-         |Date: Mon, 23 May 2005 22:38:34 GMT
-         |Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)
-         |Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT
-         |Etag: "3f80f-1b6-3e1cb03b"
-         |Content-Type: text/html; charset=UTF-8
-         |Transfer-Encoding: chunked
-         |Connection: close
-         |
-         |6
-         |Some l
-         |1a
-         |onger text:
-         |including LFs...
-         |
-         |""".stripMargin
 
       val longText =
       """|6
@@ -172,10 +211,28 @@ class InterleavedTest extends FileDiffSuite {
          |
          |""".stripMargin
 
+      ParserWorld.codegen.emitSource(
+        testChunked _,
+        "testChunked",
+        new java.io.PrintWriter(System.out)
+      )
+
       val testcChunked = ParserWorld.compile(testChunked)
       testcChunked("0\n".toArray)
       testcChunked("2\nhe\n3\nllo\n0\n\n".toArray)
       testcChunked(longText.toArray)
+
+/*
+      ParserWorld.codegen.emitSource(
+        testInterleaved _,
+        "testInterleaved",
+        new java.io.PrintWriter(System.out)
+      )
+
+      val testcInterleaved = ParserWorld.compile(testInterleaved)
+      testcInterleaved("0\n".toArray)
+      testcInterleaved("2\nhe\n3\nllo\n0\n\n".toArray)
+      testcInterleaved(longText.toArray)*/
     }
     assertFileEqualsCheck(prefix+"interleaved")
   }
